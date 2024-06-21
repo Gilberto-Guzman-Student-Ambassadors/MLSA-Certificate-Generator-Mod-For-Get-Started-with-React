e3784ed39c97784d359107b2ca9b8c6f9237b565
Gilberto-Guzman <josegilbertoguzmangutierrez@gmail.com>
2024-06-21 12:55:03 -0600

    Modificación de valores de Prueba

---
+++ NOTES.md
@@ -0,0 +1,15 @@
+# Paquetes de instalación - Installation packages.
+
+---
+
+    python --version
+    python.exe -m pip install --upgrade pip o también... pip install --upgrade pip
+
+    pip install virtualenv
+    python -m venv .venv
+    .\.venv\Scripts\Activate o también... source .venv/bin/activate
+
+    pip install docx
+    pip install docx2pdf
+
+    pip install <<biblioteca>>
--- main_certificate.py
+++ main_certificate.py
@@ -16,12 +16,13 @@ except OSError:
 
 
 def get_participants(f):
-    data = [] # create empty list
-    with open(f, mode="r", encoding='utf-8') as file:
+    data = []  # create empty list
+    with open(f, mode="r", encoding="utf-8") as file:
         csv_reader = csv.DictReader(file)
         for row in csv_reader:
-            data.append(row) # append all results
+            data.append(row)  # append all results
     return data
+
 
 def create_cert_files(filename, list_participants, host_Name, event_Name, event_Date):
 
@@ -34,36 +35,35 @@ def create_cert_files(filename, list_participants, host_Name, event_Name, event_
         event_name(doc, event_Name)
         date_of_event(doc, event_Date)
         host_name(doc, host_Name)
-        doc.save('Output/Doc/{}.docx'.format(name))
+        doc.save("Output/Doc/{}.docx".format(name))
 
         # ! if your program working slowly, comment this two line and open other 2 line.
         print("Output/{}.pdf Creating".format(name))
-        convert('Output/Doc/{}.docx'.format(name), 'Output/Pdf/{}.pdf'.format(name))
+        convert("Output/Doc/{}.docx".format(name), "Output/Pdf/{}.pdf".format(name))
 
         # ! Open those lines and comment above 2 lines if your program working extremely slow
         # os.system("docx2pdf Output/Doc/")
         # os.system("move Output\Doc\*pdf Output\PDF")
 
-    
+
 # get certificate temple path
 certificate_file = "Templates/Certificate Template.docx"
 # get participants path
 participants_file = "Templates/Event Participants.csv"
 
 # Enter your name here [Ambassador Name]
-host_Name = "Your Name"
+host_Name = "José Gilberto Guzmán Gutiérrez"
 
-#Enter event name
-event_Name = "Your Event Name"
+# Enter event name
+event_Name = "Get Started with React"
 
-#Enter event date
-event_date = "Your Event Date"
+# Enter event date
+event_date = "June 20th, 2024"
 
 # get participants
 list_participants = get_participants(participants_file)
 
 # process data
-create_cert_files(certificate_file, list_participants, host_Name, event_Name, event_date)
-
-
-
+create_cert_files(
+    certificate_file, list_participants, host_Name, event_Name, event_date
+)
